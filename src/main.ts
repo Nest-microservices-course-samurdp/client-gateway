5a01c47dfe21b174eb6d02ddd03e6c0ff13d56c7
Samuel <samuelviloriadd@gmail.com>
2024-10-03 15:42:11 -0500

    feat: console log

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,8 @@ async function bootstrap() {
   
   await app.listen(envs.PORT);
 
+  console.log("Hello world - first change")
+
   logger.log("Gateway runnin on port " + envs.PORT);
 }
 bootstrap();
